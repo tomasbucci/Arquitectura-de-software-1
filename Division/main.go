d136fc18430334a649411bfa8eb115d317bb4aea
Tomas Bucci <tomibucci@gmail.com>
2022-04-06 14:42:12 -0300

    Update main.go

--- Division/main.go
+++ Division/main.go
@@ -2,12 +2,26 @@ package div
 
 package main
 
+
 import (
 	"fmt"
+	"github.com/tomasbucci/Arquitectura-de-software-1/tree/ejercicio-http/Division/div"
 )
 
 func main() {
+	defer func() {
+		fmt.Println("Bye!")
+	}()
 
-	fmt.Println(division(8, 4))
+	a, b := float64(20), float64(10)
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
 
+	resultDiv, err := div.Division(a, b)
+	if err != nil {
+		fmt.Println("Error in division: ", err.Error())
+		return
+	}
+	fmt.Println("Div: ", resultDiv)
+	
 }
