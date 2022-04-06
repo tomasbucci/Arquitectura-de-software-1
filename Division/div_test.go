d55b93045eaf16d3882ef409387068e46d253585
Tomas Bucci <tomibucci@gmail.com>
2022-04-06 14:42:10 -0300

    Update div_test.go

--- Division/div_test.go
+++ Division/div_test.go
@@ -1,25 +1,49 @@
 package div
 
-import "testing"
+import (
+	"testing"
 
-func TestDivisionOK(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
 
-	_, err := Division(8, 4)
-
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
+type TestCase struct {
+	Name           string
+	A              float64
+	B              float64
+	ExpectedResult float64
+	ExpectedError  bool
 }
 
-func TestDivisionErrorByZero(t *testing.T) {
-
-	_, err := Division(8, 0)
-
-	if err == nil {
-		t.Error(err)
-		return
+func TestDivision(t *testing.T) {
+	for _, testCase := range []TestCase{
+		{
+			Name:           "8/4 OK",
+			A:              8,
+			B:              4,
+			ExpectedResult: 2,
+			ExpectedError:  false,
+		},
+		{
+			Name:           "9/2 OK",
+			A:              9,
+			B:              2,
+			ExpectedResult: 4.5,
+			ExpectedError:  false,
+		},
+		{
+			Name:          "9/0 Error",
+			A:             9,
+			B:             0,
+			ExpectedError: true,
+		},
+	} {
+		t.Run(testCase.Name, func(t *testing.T) {
+			result, err := Division(testCase.A, testCase.B)
+			if testCase.ExpectedError {
+				assert.NotNil(t, err)
+				return
+			}
+			assert.EqualValues(t, testCase.ExpectedResult, result)
+		})
 	}
-
 }
